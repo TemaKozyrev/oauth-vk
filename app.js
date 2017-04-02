697fcfe0d97eb5f1eaedcdb90bd3a8415210abb3
TemaKozyrev <tema@tema.ws>
2017-04-02 12:51:55 +0300

    vkapi - get 5 friends

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var VK = require('vksdk');
 
 var passport = require('passport');
 const VKontakteStrategy = require('passport-vkontakte').Strategy;
@@ -44,17 +44,38 @@ passport.use(new VKontakteStrategy(
         clientID: VKONTAKTE_APP_ID,
         clientSecret: VKONTAKTE_APP_SECRET,
         callbackURL: "http://localhost:3000/auth/vk/callback",
-        scope: ['email', 'friends'],
-        profileFields: ['email', 'friends'],
+        scope: ['friends']
+        // profileFields: ['email', 'friends'],
     },
     function verify(accessToken, refreshToken, params, profile, done) {
-        process.nextTick(function () {
-            return done(null, profile);
+        vk.setToken(accessToken);
+
+        vk.request('friends.get', {'user_id' : profile.id, 'count': 5, 'fields': ['nickname']}, function(_o) {
+            profile.friends = []
+            _o.response.items.forEach(function (elem) {
+                profile.friends.push(elem.first_name + ' ' + elem.last_name)
+            })
+            process.nextTick(function () {
+                console.log(profile.friends);
+                return done(null, profile);
+            });
         });
+
     }
 ));
 
+// vk api
 
+var vk = new VK({
+    'appId'     : VKONTAKTE_APP_ID,
+    'appSecret' : VKONTAKTE_APP_SECRET,
+    'language'  : 'ru'
+});
+
+
+// Setup server access token for server API methods
+
+// Turn on requests with access tokens
 
 app.use(require('./controllers'));
 
