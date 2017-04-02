2e40716d7450c43a66c3922b8aa405b5b24feca7
TemaKozyrev <tema@tema.ws>
2017-04-02 11:53:24 +0300

    change template + init controller, strategy

--- app.js
+++ app.js
@@ -6,20 +6,55 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var passport = require('passport');
+const VKontakteStrategy = require('passport-vkontakte').Strategy;
+
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('express-session')({secret:'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// vk app config
+const VKONTAKTE_APP_ID = '5961288';
+const VKONTAKTE_APP_SECRET = 'CuYYBlGdVvDuk5PqPzBP';
+
+// passport js config
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new VKontakteStrategy(
+    {
+        clientID: VKONTAKTE_APP_ID,
+        clientSecret: VKONTAKTE_APP_SECRET,
+        callbackURL: "http://localhost:3000/auth/vk/callback",
+        scope: ['email', 'friends'],
+        profileFields: ['email', 'friends'],
+    },
+    function verify(accessToken, refreshToken, params, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
+
+
 
 app.use(require('./controllers'));
 
