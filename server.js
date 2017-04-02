e7c1bc3849dbff46ce7bfa366af5cae378b10f27
TemaKozyrev <tema@tema.ws>
2017-04-02 22:14:24 +0300

    fix server.js, add nginx config

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var VK = require('vksdk');
+var http = require('http');
 
 var passport = require('passport');
 const VKontakteStrategy = require('passport-vkontakte').Strategy;
@@ -79,5 +80,27 @@ var vk = new VK({
 
 app.use(require('./controllers'));
 
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-module.exports = app;
+var server = http.createServer(app);
+
+server.listen(port);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+
